35a52b12eb7f6c398b19216c3ad02d75af31347e
Benjamin Ikeh <b.ikeh@yahoo.com>
2022-05-23 15:27:44 +0000

    add properties to handle password filter and username as well

--- src/main/java/io/benyon/springsecurityjwt/SecurityConfigurer.java
+++ src/main/java/io/benyon/springsecurityjwt/SecurityConfigurer.java
@@ -1,5 +1,6 @@
 package io.benyon.springsecurityjwt;
 
+import io.benyon.springsecurityjwt.filters.JwtRequestFilter;
 import io.benyon.springsecurityjwt.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,14 +9,19 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
   
   @Autowired
   private final MyUserDetailsService myUserDetailsService;
+  
+  @Autowired
+  private JwtRequestFilter jwtRequestFilter;
 
   public SecurityConfigurer(MyUserDetailsService myUserDetailsService) {
     this.myUserDetailsService = myUserDetailsService;
@@ -29,7 +35,10 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception{
     http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll()
-      .anyRequest().authenticated();
+      .anyRequest().authenticated()
+      .and().sessionManagement()
+      .sessionCreationPolicy(SessionCreationPolicy.STATELESS); 
+    http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
   }
   
   @Override
